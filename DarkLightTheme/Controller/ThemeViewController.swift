1606a6938da7cd34ff7e769be44823be5dea2c10
lazdovska <lazdovska.kristine@gmail.com>
2021-07-21 20:24:40 +0300

    DarkLightTheme

---
+++ DarkLightTheme/Controller/ThemeViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ThemeViewController.swift
+//  DarkLightTheme
+//
+//  Created by kristine.lazdovska on 21/07/2021.
+//
+
+import UIKit
+
+class ThemeViewController: UIViewController {
+    
+    
+    @IBOutlet weak var darkButton: UIButton!
+    var isOn = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ThemeViewController")
+       
+    }
+
+    
+    @IBAction func darkButtonTapped(_ sender: Any) {
+    if isOn {
+        print("Dark theme on")
+        self.isOn = false
+        self.darkButton.setTitle("Dark theme on", for: .normal)
+        self.darkButton.setTitleColor(UIColor.white, for: .normal)
+        self.view.backgroundColor = .white
+    }else{
+        print("Dark theme off")
+        self.isOn = true
+        self.darkButton.setTitle("Dark theme off", for: .normal)
+        self.darkButton.setTitleColor(UIColor.black, for: .normal)
+        self.view.backgroundColor = .black
+    }
+    }
+    
+}
